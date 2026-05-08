1693bbeee1facd7db3604b222434a1eab494e8d1
Werych-mm <werychm@gmail.com>
2026-05-08 11:16:46 -0300

    Add files via upload

--- Saude/src/app/index.tsx
+++ Saude/src/app/index.tsx
@@ -21,10 +21,10 @@ export default function Cadastro() {
                 <TextInput style={styles.iCPF}/>
 
                 <Text style={styles.Senha}>Senha:</Text>
-                <TextInput style={styles.Senha}/>
+                <TextInput style={styles.iSenha}/>
 
                 <Text style={styles.ConSenha}>Confirmar Senha:</Text>
-                <TextInput style={styles.ConSenha}/>
+                <TextInput style={styles.iConSenha}/>
 
                 <Text style={styles.Data}>Data de Nascimento:</Text>
                 <TextInput placeholder='dd/mm/aaaa' style={styles.iData}/>
@@ -52,14 +52,116 @@ const styles = StyleSheet.create({
         display: "flex",
         flexDirection: "row",
         alignItems: "center",
-        border: "1px solid black",
+        marginBottom: "2%"
     },
 
     titulo: {
         color: "#FFF",
         fontSize: "75px",
         fontWeight: "Bold",
-        height: "100%"
+    },
+
+    logo: {
+        width: "30%",
+        height: "75%"
+    },
+
+    card: {
+        backgroundColor: "#FFF",
+        width: "55%",
+        height: "60%",
+        borderRadius: "10px"
+    },
+
+    nome: {
+        color: "#656565",
+        marginTop: "25px",
+        marginLeft: "10px"
+    },
+
+    inome: {
+        border: "1px solid #656565",
+        borderRadius: "5px",
+        marginLeft: "10px",
+        padding: "8px",
+        width: "90%"
+    },
+
+    email: {
+        color: "#656565",
+        marginTop: "10px",
+        marginLeft: "10px"
+    },
+
+    iemail: {
+        border: "1px solid #656565",
+        borderRadius: "5px",
+        marginLeft: "10px",
+        padding: "8px",
+        width: "90%"
+    },
+
+    CPF: {
+        color: "#656565",
+        marginTop: "10px",
+        marginLeft: "10px"
+    },
+
+    iCPF: {
+        border: "1px solid #656565",
+        borderRadius: "5px",
+        marginLeft: "10px",
+        padding: "8px",
+        width: "90%"
+    },
+
+    Senha: {
+        color: "#656565",
+        marginTop: "10px",
+        marginLeft: "10px"
+    },
+
+    iSenha: {
+        border: "1px solid #656565",
+        borderRadius: "5px",
+        marginLeft: "10px",
+        padding: "8px",
+        width: "90%"
+    },
+
+    ConSenha: {
+        color: "#656565",
+        marginTop: "10px",
+        marginLeft: "10px"
+    },
+
+    iConSenha: {
+        border: "1px solid #656565",
+        borderRadius: "5px",
+        marginLeft: "10px",
+        padding: "8px",
+        width: "90%"
+    },
+
+    Data: {
+        color: "#656565",
+        marginTop: "10px",
+        marginLeft: "10px"
+    },
+
+    iData: {
+        border: "1px solid #656565",
+        borderRadius: "5px",
+        marginLeft: "10px",
+        padding: "8px",
+        width: "90%"
+    },
+
+    botao: {
+        backgroundColor: ""
     }
+
+
+
 
 })
